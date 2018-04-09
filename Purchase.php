d584267efc35c11c28080fa83eee0ab7572900d0
vasq5627 <35650363+vasq5627@users.noreply.github.com>
2018-04-09 12:09:29 -0700

    Create Purchase.php

---
+++ Purchase.php
@@ -0,0 +1,56 @@
+<?php
+    //creating session
+    session_start();
+    
+    
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <title> Cart </title>
+         <style>
+            @import url(css/styl.css);
+        </style>
+    </head>
+    <body>
+        <form></form>
+        <h1> Shopping Cart </h1>
+          <div class='container'>
+            <div class='text-center'>
+                
+                <!-- Bootstrap Navagation Bar -->
+                <nav class='navbar navbar-default - navbar-fixed-top'>
+                    <div class='container-fluid'>
+                        <div class='navbar-header'>
+                            <a class='navbar-brand' href='#'>Shopping Land</a>
+                        </div>
+                        <ul class='nav navbar-nav'>
+                            <li><a href='index.php'>Home</a></li>
+                            <li><a href='results.php'>
+                            <span class='glyphicon glyphicon-shopping-cart' aria-hidden='true'>
+                                </span> </a></li>
+                        </ul>
+                    </div>
+                </nav>
+                <br /> <br /> <br />
+                <h2> Shopping Cart </h2>
+            </div>
+        </div>
+        </form>
+
+    </body>
+    <footer id="footer">
+            <hr>
+            CST336 Internet Programming. 2018 &copy <br />
+            <strong> Disclaimer: </strong>
+            The information in this website is fictitous. It's used for academic purposes.
+            <a href="http://csumb.edu">CSUMB</a>
+            <figure>
+                <img src="img/Monterey.jpg" alt="School Logo" height="100" width="150" />
+            </figure>
+            
+            
+        </footer>
+</html>
