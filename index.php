74e1ca913415a3e1d7b0b6975de9fdad74c7b79c
vasq5627 <35650363+vasq5627@users.noreply.github.com>
2018-04-10 12:04:02 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-
 <?php
 //creating session
-
     session_start();
     $_SESSION['searches']= array();
    /* if(!isset($_SESSION['cart'])){
-
         $_SESSION['cart'] = array();
 
     }*/
@@ -31,27 +28,35 @@
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
     <body>
-                <h1> Video Game Market <img src="img/videog.png"  width="50" height="50"/> </h1>
+             
+                 <h1 id="title"> Video Game Market <img src="img/videog.png"  width="50" height="50"/> </h1>
+               
         <br>
+        
          <img src="img/robot.png" id="robot"  width="150" height="250"/>
        <img src="img/control.png"  id="controller" width="150" height="250"/>
         <form action="results.php">
           
             
             <br />
-           
+           <button type="button" id='cart' class="btn btn-success">
+ <a href='Purchase.php'> Shopping Cart <span class="badge badge-dark"></span>
+  <span class="sr-only">unread messages</span>
+</button> </a>
+    <br />
+    <br />
             <h3>Video Game: </h3> 
             <input type="text" name="game" placeholder="Search" value="<?=$_GET['game']?>"/> <br /> <br />
             
          <br>
             <h3 >Game Category: </h3> 
-            <select name="category" >
-                <option value="">Select One</option>
-                <option value="action">Action-adventure</option></option>
-                <option value="shooter">First-person Shooter</option></option>
+            <select name="category" class="custom-select">
+                <option selected>Select One</option>
+                <option value="action">Action</option></option>
+                <option value="shooter">Shooter</option></option>
                 <option value="fantasy">Fantasy</option></option>
                 <option value="sports">Sports</option></option>
-                <option value="Puzzle">Puzzle</option></option>
+                <option value="role">Role-Play</option></option>
             </select>
             <br>
             <br>
@@ -59,11 +64,11 @@
             
           <h3 >Game Platform: </h3>
           <div class="btn-group btn-group-toggle" data-toggle="buttons">
-              <label class="btn btn-secondary active">
-                <input type="radio" name="options" id="option1" autocomplete="off"> Xbox One 
+              <label class="btn btn-success">
+                <input type="radio" name="options" id="optionOne" autocomplete="off"> Xbox One 
                   </label>
-              <label class="btn btn-secondary">
-                <input type="radio" name="options" id="option2" autocomplete="off"> PS4
+              <label class="btn btn-primary">
+                <input type="radio" name="options" id="optionTwo" autocomplete="off"> PS4
           </label>
             </div>
             <br>
@@ -71,19 +76,30 @@
             <br>
             
             
-           <h3> Price Range: <br> From</h3> <input type="text" name="priceFrom" placeholder="Min" size="4"/>
+           <h3> Price Range: <br> <br /> From</h3> <input type="text" name="priceFrom" placeholder="Min" size="4"/>
                  <h3>   To   </h3>  <input type="text" name="priceTo" placeholder="Max" size="4"/>
                 <br>
+                <br />
                 <h3> Order By: </h3>
-                <input class="form-check-input" type="radio" name="exampleRadio" id="Radios1" value="option1">
-              <label class="form-check-label" for="Radios1">Price</label>
-                </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="exampleRadio" id="exampleRadios2" value="option2">
-              <label class="form-check-label" for="Radios2"> Name </label>
+               <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                  <label class="btn btn-danger">
+                <input type="radio" name="options" id="option1" autocomplete="off"> Cost
+              </label>
+              <label class="btn btn-info">
+                <input type="radio" name="options" id="option2" autocomplete="off"> Game Title
+              </label>
+            </div>
            <br>
            <br>
+           <br />
              <input type="submit" value="Search" name="searchForm" />
+             <br>
+             <br />
+             <br>
+            <h4> Progress Bar </h4>
+             <div class="progress"> 
+  <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
         </form>
        
     </div>
