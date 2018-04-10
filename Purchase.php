cb662054a75db91168d749c7c228b57e0d31c499
vasq5627 <35650363+vasq5627@users.noreply.github.com>
2018-04-10 12:04:59 -0700

    Update Purchase.php

--- Purchase.php
+++ Purchase.php
@@ -15,29 +15,9 @@
         </style>
     </head>
     <body>
-        <form></form>
         <h1> Shopping Cart </h1>
-          <div class='container'>
-            <div class='text-center'>
-                
-                <!-- Bootstrap Navagation Bar -->
-                <nav class='navbar navbar-default - navbar-fixed-top'>
-                    <div class='container-fluid'>
-                        <div class='navbar-header'>
-                            <a class='navbar-brand' href='#'>Shopping Land</a>
-                        </div>
-                        <ul class='nav navbar-nav'>
-                            <li><a href='index.php'>Home</a></li>
-                            <li><a href='results.php'>
-                            <span class='glyphicon glyphicon-shopping-cart' aria-hidden='true'>
-                                </span> </a></li>
-                        </ul>
-                    </div>
-                </nav>
-                <br /> <br /> <br />
-                <h2> Shopping Cart </h2>
-            </div>
-        </div>
+        <form>
+            
         </form>
 
     </body>
