de21b4739a56fe137bb8d7eb03590fe8b1e194d7
vasq5627 <35650363+vasq5627@users.noreply.github.com>
2018-04-11 08:47:38 -0700

    Update purchaseHistory.php

--- purchaseHistory.php
+++ purchaseHistory.php
@@ -1,35 +1,32 @@
 <?php
     
-    include 'dbConnection.php';
+    include 'dbConn.php';
     $conn = getDatabaseConnection("Gamestore");
-    $productId = $_GET['ID'];
+    $iD = $_GET['ID'];
     $sql = "SELECT * FROM `PRICE`
             NATURAL JOIN GENRE 
             WHERE PRICE.ID = GENRE.ID";    
     
     $np = array();
-    $np[":pId"] = $productId;
+    $np[":Id"] = $ID;
     
     $stmt = $conn->prepare($sql);
     $stmt->execute($np);
     $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    //print_r($records);
+   
     
     foreach ($records as $record) {
         
-        echo $record['productName'] . "<br>";
-        echo "<img src='" . $record['productImage'] . "' width = '200' /><br/>";
-        echo "Purchase Date: " . $record["purchaseDate"] . "<br />";
-        echo "Unit Price: " . $record["unitPrice"] . "<br />";
-        echo "Quantity: " . $record["quantity"] . "<br />";
+        echo $record[0]['Title'] . "<br>";
+        echo "<img src='" . $record[0]['gameImage'] . "' width = '200' /><br/>";
+        echo "Platform: " . $record["Platform"] . "<br />";
+        echo "Unit Price: " . $record["Price"] . "<br />";
+        echo "Genre: " . $record["Genre"] . "<br />";
         
         
      
     }
     
-    if(sizeof($records) == 0) {
-        echo "No purchase information found, please direct back and choose another item!";
-    }
  ?>
 
 <!DOCTYPE html>
